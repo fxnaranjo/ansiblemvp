c8f599b727ca2c1f27389b08432d40e5031b498f
Francisco Naranjo <fxnaranjo@gmail.com>
2022-01-21 23:42:05 -0500

    fix

--- projects/copyProjects2.sh
+++ projects/copyProjects2.sh
@@ -10,17 +10,17 @@ cd /root/ansible
 
 git clone https://github.com/fxnaranjo/ansiblemvp.git
 
-cp -R /root/ansible/projects/collections/ /ansible/dcaproject
+cp -R /root/ansible/ansiblemvp/projects/collections/ /ansible/dcaproject
 cp -R /root/ansible/projects/roles/ /ansible/dcaproject
 cp -R /root/ansible/projects/group_vars/ /ansible/dcaproject
 
-cp /root/ansible/projects/lpar-off-main.yml /ansible/dcaproject
-cp /root/ansible/projects/lpar-on-main.yml /ansible/dcaproject
-cp /root/ansible/projects/csm-main-sgm.yml /ansible/dcaproject
-cp /root/ansible/projects/csm-main-rgm.yml /ansible/dcaproject
-cp /root/ansible/projects/move-to-dca.yml /ansible/dcaproject
+cp /root/ansible/ansiblemvp/projects/lpar-off-main.yml /ansible/dcaproject
+cp /root/ansible/ansiblemvp/projects/lpar-on-main.yml /ansible/dcaproject
+cp /root/ansible/ansiblemvp/projects/csm-main-sgm.yml /ansible/dcaproject
+cp /root/ansible/ansiblemvp/projects/csm-main-rgm.yml /ansible/dcaproject
+cp /root/ansible/ansiblemvp/projects/move-to-dca.yml /ansible/dcaproject
 
-cp /root/ansible/projects/vault-pass /ansible/dcaproject
+cp /root/ansible/ansiblemvp/projects/vault-pass /ansible/dcaproject
 
 
 chmod -R 774 /ansible
